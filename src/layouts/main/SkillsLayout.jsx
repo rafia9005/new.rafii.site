a4f8fe64862bb4eb82c9ceb41893121bd500596c
rafia9005 <rafia9005@gmail.com>
2023-12-02 23:35:48 +0700

    oke

--- src/layouts/main/SkillsLayout.jsx
+++ src/layouts/main/SkillsLayout.jsx
@@ -9,13 +9,21 @@ export default function SkillsLayout() {
             </h1>
             <img src="arrow-style.svg" alt="" className="rotate-[90deg] lg:w-[100px] w-[50px]"/>
           </div>
-          <div className="flex items-center gap-[30px] justify-start lg:px-0 px-[10px] mt-4">
-            <img src="nextjs.svg" alt="" className="lg:w-[100px] w-[40px]"/>
-            <img src="tailwind.png" alt="" className="lg:w-[100px] w-[40px]"/>
-            <img src="nest.png" alt="" className="lg:w-[100px] w-[40px]"/>
-            <img src="django.png" alt="" className="lg:w-[100px] w-[40px]"/>
-            <img src="golang.png" alt="" className="lg:w-[100px] w-[40px]"/>
-            <img src="vue.png" alt="" className="lg:w-[100px] w-[40px]"/>
+          <div className="flex items-center gap-[30px] justify-start lg:px-0 px-[10px] mt-[60px]">
+            <img src="nextjs.svg" alt="" className="lg:w-[100px] w-[40px] rounded-lg"/>
+            <img src="tailwind.png" alt="" className="lg:w-[100px] w-[40px] rounded-lg"/>
+            <img src="nest.png" alt="" className="lg:w-[100px] w-[40px] rounded-lg"/>
+            <img src="django.png" alt="" className="lg:w-[100px] w-[40px] rounded-lg"/>
+            <img src="golang.png" alt="" className="lg:w-[100px] w-[40px] rounded-lg"/>
+            <img src="vue.png" alt="" className="lg:w-[100px] w-[40px] rounded-lg"/>
+          </div>
+          <div className="flex items-center gap-[30px] justify-start lg:px-0 px-[10px] mt-[60px]">
+            <img src="angular.png" alt="" className="lg:w-[100px] w-[40px] rounded-lg"/>
+            <img src="svelte.png" alt="" className="lg:w-[100px] w-[40px] rounded-lg"/>
+            <img src="typescript.png" alt="" className="lg:w-[100px] w-[40px] rounded-lg"/>
+            <img src="docker.png" alt="" className="lg:w-[100px] w-[40px] rounded-lg"/>
+            <img src="laravel.png" alt="" className="lg:w-[100px] w-[40px] rounded-lg"/>
+            <img src="react.png" alt="" className="lg:w-[100px] w-[40px] rounded-lg"/>
           </div>
         </div>
       </div>
