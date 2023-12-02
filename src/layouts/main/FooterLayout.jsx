f97af21cd843ee8365e1b74404f8a1b9bacc459f
rafia9005 <rafia9005@gmail.com>
2023-12-03 06:56:32 +0700

    baru selesai

--- src/layouts/main/FooterLayout.jsx
+++ src/layouts/main/FooterLayout.jsx
@@ -4,7 +4,7 @@ export default function FooterLayout(){
             <footer className="lg:flex justify-evenly items-center mt-[40px] h-[40vh] block px-[10%]">
                 <div>
                 <h1 className="lg:text-[80px] text-3xl"><span className="lg:text-[120px] text-[40px]">C</span>ontact</h1>
-                <h1 className="text-xl">email : <b>rafia9005@gmail.com</b></h1>
+                <h1 className="text-xl mt-5">email : <b>rafia9005@gmail.com</b></h1>
                 <h1>instagram : <b>@rafii.site</b></h1>
                 </div>
                 <ul className="lg:block lg:text-start text-center lg:mt-0 mt-5">
